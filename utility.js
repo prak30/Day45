44e497ff04da3ecd86eacb862fb0a1c31c4ab63f
pranav <prnvkatkar88@gmail.com>
2022-01-05 20:30:42 +0530

    added regex

--- utility.js
+++ utility.js
@@ -5,4 +5,17 @@ const stringifyDate = (date) => {
     return newDate;
 }
 
+const checkName = (name) => {
+    let nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$');
+    if (!nameRegex.test(name)) throw "Name is Incorrect";
+  }
+  
+  const checkStartDate = (startDate) => {
+    let now = new Date();
+    if(startDate>now) throw 'start date is a future date';
+    var diff = Math.abs(now.getTime()-startDate.getTime());
+    if(diff/(1000*60*69*24)>30)
+        throw 'start date is beyond 30days';
+  }
+
 
