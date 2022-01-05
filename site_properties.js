1ec0ba8d52196e10ac38ccd417da68f0c580509d
pranav <prnvkatkar88@gmail.com>
2022-01-05 20:30:07 +0530

    set use local storage as true

--- site_properties.js
+++ site_properties.js
@@ -1,4 +1,6 @@
 let site_properties = {
+    use_local_storage: "true",
     home_page: "home.html",
-    add_emp_payroll_page: "emp.html"
-};
+    add_emp_payroll_page: "emp.html",
+    server_url:"http://127.0.01.3000/employeePayrollDB" 
+  };
